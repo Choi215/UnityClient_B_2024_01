4e5092f6430f885f11620c19a2571fc7d87ec3fa
GangSan <ggomarain1302@gmail.com>
2024-05-02 10:13:12 +0900

    [feat] 게임 시스템 추가(코드 미완)

--- 001project/Assets/ExEnemyManager.cs
+++ 001project/Assets/ExEnemyManager.cs
@@ -1,37 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Linq;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using System.Linq;
 
-public class ExEnemyManager : MonoBehaviour
-{
-    public List<ExEnemy> enemies = new List<ExEnemy>();
+//public class ExEnemyManager : MonoBehaviour
+//{
+//    public List<ExEnemy> enemies = new List<ExEnemy>();
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        //데미지가 낮은 순서대로 적 캐릭터 정렬
-        var sortedEnemies = enemies.OrderBy(enemy => enemy.damage);
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        //데미지가 낮은 순서대로 적 캐릭터 정렬
+//        var sortedEnemies = enemies.OrderBy(enemy => enemy.damage);
 
 
-        foreach (var enemy in sortedEnemies)
-        {
-            Debug.Log(enemy"Sorted Enemy : " + enemy.gameObject.name + "Damage : " + enemy.damage);
-        }
+//        foreach (var enemy in sortedEnemies)
+//        {
+//            Debug.Log(enemy"Sorted Enemy : " + enemy.gameObject.name + "Damage : " + enemy.damage);
+//        }
 
-        //특정 거리 이내의 적 캐릭터 선택
-        float maxDistance = 10f;
-        var closeEnemies = enemies.Where(enemy => Vector3.Distance(enemy.transform.position, transform.position) < maxDistance);
+//        //특정 거리 이내의 적 캐릭터 선택
+//        float maxDistance = 10f;
+//        var closeEnemies = enemies.Where(enemy => Vector3.Distance(enemy.transform.position, transform.position) < maxDistance);
 
-        foreach (var enemy in closeEnemies)
-        {
-            Debug.Log("Close Enemies : " + enemy.gameObject.name);
-        }
-    }
+//        foreach (var enemy in closeEnemies)
+//        {
+//            Debug.Log("Close Enemies : " + enemy.gameObject.name);
+//        }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
         
-    }
-}
+//    }
+//}
